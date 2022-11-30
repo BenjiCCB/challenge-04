08cc1264d18b2916347cfde2dcb284c05d9dc763
BenjiCCB <bensdecker+CCB@gmail.com>
2022-11-30 18:42:15 -0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var startButton = document.querySelector(".start-button");
+var introPage = document.querySelector("#intro");
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+  
+}
